dc28e53e700c774e9c173e593e3262f54bb35f69
Bastien <bast.reynard@gmail.com>
2024-06-05 08:24:49 +0000

    Empty cron log every day, update TZ

--- show_db.sh
+++ show_db.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 export MYSQL_USER="root"
 export MYSQL_PASSWORD="_sqlrootpassword_"
+if [ -z "$1" ]; then
+echo "Possible args are \"geo\" or \"time\""
+exit 0;
+fi
+if [ $1 == "geo" ]; then
 docker exec -it mysql-f2b-geo-export mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -Dbanned_ips_db -e "SELECT * FROM banned_ips"
 docker exec -it mysql-f2b-geo-export mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -Dbanned_ips_db -e "SELECT COUNT(*) FROM banned_ips"
+elif [ $1 == "time" ]; then
+docker exec -it mysql-f2b-geo-export mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -Dbanned_ips_db -e "SELECT * FROM num_bans"
+fi
